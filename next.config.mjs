64b4a6e30e866809a84d887a33fcd3686d6e2b7a
Ernesto Fava <e.fava13@gmail.com>
2024-10-04 15:33:40 -0400

    Exportación estática de NextJS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    /*Dado que Next.js admite esta exportación estática, 
+    se puede implementar y alojar en cualquier servidor web 
+    que pueda servir activos estáticos HTML/CSS/JS.*/
+    output: "export",
+};
 
 export default nextConfig;
