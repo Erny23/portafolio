a4cc8aa9c753a3b2e6205a6b8ad87edced531b0f
Ernesto Fava <e.fava13@gmail.com>
2024-11-22 01:43:40 -0400

    Corrección
    La importación fallo en producción debido a unas letras en mayúsculas.

--- app/ui/navbar/options/index.ts
+++ app/ui/navbar/options/index.ts
@@ -1,4 +1,4 @@
 /* Barras de navegación */
-export { default as desktop } from "./Desktop";
+export { default as desktop } from "./desktop";
 export { default as tablet } from "./tablet";
-export { default as movil } from "./Movil";
+export { default as movil } from "./movil";
