97a78db074030306d5b4afd6c707d179753975f2
Ernesto Fava <e.fava13@gmail.com>
2024-12-04 15:34:10 -0400

    Hover en botones
    Efecto hover en botones para pc y no en móvil.

--- app/ui/navbar/options/desktop.tsx
+++ app/ui/navbar/options/desktop.tsx
@@ -14,19 +14,19 @@ const Desktop = () => {
     return (
         <>
             <ul className="fixed top-12 left-3 grid gap-3 divide-y-2 bg-white rounded-md w-24 p-3 lg:absolute lg:top-6 lg:-left-28">
-                <li className={`${router === "/" ? "text-complementary" : null} pt-3`}>
+                <li className={`${router === "/" ? "text-complementary" : null} pt-3 lg:rounded-md lg:hover:bg-gray-200`}>
                     <Link href="/">
                         <FaUser className="w-full" />
                         <h3 className="text-center">Perfil</h3>
                     </Link>
                 </li>
-                <li className={`${router === "/works" ? "text-complementary" : null} pt-3`}>
+                <li className={`${router === "/works" ? "text-complementary" : null} pt-3 lg:rounded-md lg:hover:bg-gray-200`}>
                     <Link href="/works">
                         <FaCode className="w-full" />
                         <h3 className="text-center">Trabajos</h3>
                     </Link>
                 </li>
-                <li className={`${router === "/contact" ? "text-complementary" : null} py-3`}>
+                <li className={`${router === "/contact" ? "text-complementary" : null} py-3 lg:rounded-md lg:hover:bg-gray-200`}>
                     <Link href="/contact">
                         <BiSolidContact className="w-full" />
                         <h3 className="text-center">Contacto</h3>
